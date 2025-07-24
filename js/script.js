4ab996b330e22fa684f371ed508196ff39d8d726
bluexox <hellodwya@naver.com>
2025-07-24 10:55:07 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 $(document).ready(function () {
 
+    document.addEventListener("DOMContentLoaded", function () {
+        const form = document.getElementById("inquiryForm");
+        const submitBtn = document.querySelector(".inquiry-btn button");
+
+        submitBtn.addEventListener("click", function (e) {
+            const name = form.querySelector("input[name='name']").value.trim();
+            const phone = form.querySelector("input[name='phone']").value.trim();
+            const area = form.querySelector("input[name='area']").value.trim();
+            const message = form.querySelector("textarea[name='message']").value.trim();
+
+            if (!name || !phone || !area || !message) {
+                alert("모든 항목을 입력해 주세요.");
+                e.preventDefault(); // 폼 전송 방지
+            }
+        });
+    });
+
     const interiorSwiper = new Swiper('.interior-swiper', {
         slidesPerView: 'auto',
         spaceBetween: 20,
@@ -17,6 +34,5 @@ $(document).ready(function () {
             draggable: true,
         },
     });
-
 
 });
